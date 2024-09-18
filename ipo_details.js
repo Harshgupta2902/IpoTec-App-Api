a365bab6be8fbd40df44e858d523c63c6b5a2f36
Harsh <harsh.gupta@cognustechnology.com>
2024-09-18 14:54:18 +0530

    ipo details

---
+++ ipo_details.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+
+router.get("/:name", async (req, res) => {
+  const { name } = req.params;
+
+  const url = `https://groww.in/v1/api/stocks_primary_market_data/v1/ipo/company/${name}?isHniEnabled=true`;
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
