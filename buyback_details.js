c994dabc2c4cf8aaa645bf1512bf1a3aefd19740
Harsh <harsh.gupta@cognustechnology.com>
2024-09-19 16:08:49 +0530

    buyback details api chnages

--- buyback_details.js
+++ buyback_details.js
@@ -8,7 +8,21 @@ router.get("/:name", async (req, res) => {
   const url = `https://groww.in/v1/api/stocks_portfolio/v2/buyback/fetch?searchId=${name}`;
   try {
     const response = await axios.get(url);
-    res.json(response.data);
+    const mainData = response.data;
+
+    const searchId = mainData.data.searchId?.replace("-buyback", "").trim();
+
+    const reponse1 = await axios.get(
+      `https://groww.in/v1/api/stocks_data/v1/company/search_id/${searchId}?page=0&size=10`
+    );
+    const symnolData = reponse1.data;
+
+    const reponse2 = await axios.get(
+      `https://groww.in/v1/api/stocks_data/v1/company/search_id/${searchId}?page=0&size=10`
+    );
+    const nav = reponse1.data;
+
+    res.json({ mainData, symnolData });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
