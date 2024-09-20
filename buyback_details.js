51db1cd0917a5275a0c8cdcc6b1fe41b52554e72
Harsh <harsh.gupta@cognustechnology.com>
2024-09-20 12:53:08 +0530

    buyback api changes

--- buyback_details.js
+++ buyback_details.js
@@ -2,6 +2,26 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
+function convertShareHoldingPattern(shareHoldingPattern) {
+  return Object.keys(shareHoldingPattern).map((period) => ({
+    period: period,
+    ...shareHoldingPattern[period],
+  }));
+}
+function convertToNewFormat(financials) {
+  return financials.map((item) => {
+    const yearlyArray = Object.keys(item.yearly).map((year) => ({
+      year: parseInt(year),
+      value: item.yearly[year],
+    }));
+
+    return {
+      title: item.title,
+      yearly: yearlyArray,
+    };
+  });
+}
+
 router.get("/:name", async (req, res) => {
   const { name } = req.params;
 
@@ -9,25 +29,31 @@ router.get("/:name", async (req, res) => {
   try {
     const response = await axios.get(url);
     const mainData = response.data;
-
+    const data = mainData.data;
     const searchId = mainData.data.searchId?.replace("-buyback", "").trim();
 
     const reponse1 = await axios.get(
       `https://groww.in/v1/api/stocks_data/v1/company/search_id/${searchId}?page=0&size=10`
     );
-    const symnolData = reponse1.data;
+    const symbolData = reponse1.data;
 
+    const shareHoldingPattern = convertShareHoldingPattern(
+      symbolData.shareHoldingPattern
+    );
+    delete symbolData.shareHoldingPattern;
 
-    res.json({ mainData, symnolData });
+    const financials = convertToNewFormat(symbolData.financialStatement);
+
+    delete symbolData.financialStatement;
+    delete symbolData.brandDtos;
+    delete symbolData.fundsInvested;
+
+    res.json({ data, symbolData, shareHoldingPattern, financials });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
 module.exports = router;
-
-
-
-
 
 // Convert the financials data
