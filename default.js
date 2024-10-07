3466b9f2cebd6efe329a32a6145ec779f6666e8a
Harsh Gupta <147129512+Harshgupta2902@users.noreply.github.com>
2024-10-07 08:44:30 +0530

    Update default.js

--- default.js
+++ default.js
@@ -5,8 +5,8 @@ router.get("/", async (req, res) => {
   try {
     const response = {
       force_update: 1,
-      soft_update: 0,
-      build_no: 4,
+      soft_update: 1,
+      build_no: 5,
       ios_build_no: 1,
       title: "Update",
       message: "A New Version of App is available",
