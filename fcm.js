013d7c73028955193624222075a3304736ad9976
Harsh <harsh.gupta@cognustechnology.com>
2024-10-16 17:07:01 +0530

    chnages

--- fcm.js
+++ fcm.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { admin, db } = require("./firebase");
-const { messaging } = require("firebase-admin");
 
 router.get("/", async (req, res) => {
   const usersSnapshot = await db.collection("userData").get();
