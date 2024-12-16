d34268e5da6a32f9b4acae9ef2fba8f349d1564b
Harsh <harsh.gupta@cognustechnology.com>
2024-12-16 10:34:46 +0530

    notifications also started

--- api.js
+++ api.js
@@ -119,17 +119,15 @@ cron.schedule("0 12 * * *", async () => {
 app.listen(3001, () => {
   console.log(`Server is running on http://localhost:${3001}/app/`);
   (async () => {
-    // try {
-    //   const response = await axios.get(`http://localhost:${3001}/app/checkevents`);
-    //   console.log("Check events response:", response.data);
-    //   await checkForLatestPost();
-    //   setInterval(() => {
-    //     const currentTime = new Date().toLocaleString();
-    //     console.log(`Checking for new posts at ${currentTime}...`);
-    //     checkForLatestPost();
-    //   }, 300000);
-    // } catch (error) {
-    //   console.error("Error hitting /app/checkBlogs:", error.message);
-    // }
+    try {
+      await checkForLatestPost();
+      setInterval(() => {
+        const currentTime = new Date().toLocaleString();
+        console.log(`Checking for new posts at ${currentTime}...`);
+        checkForLatestPost();
+      }, 300000);
+    } catch (error) {
+      console.error("Error hitting /app/checkBlogs:", error.message);
+    }
   })();
 });
