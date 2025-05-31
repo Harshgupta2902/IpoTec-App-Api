be4d6d21efb053b58c5629e55606d13cf1d023ed
Harsh <gyanitalksapp@gmail.com>
2025-05-31 11:16:30 +0530

    gmp data correction

--- ipo/common/gmp.js
+++ ipo/common/gmp.js
@@ -10,11 +10,15 @@ router.get("/", async (req, res) => {
     );
 
     const ipoData = responseData.reportTableData.map((item) => {
+      const listingPrice = item["Est Listing"] ? item["Est Listing"].replace(/<[^>]*>/g, "") : "";
+
+      const gmpMatch = listingPrice.match(/\(([^)]+)\)/);
+      const gmp = gmpMatch ? gmpMatch[1] : "";
       return {
         companyName: item.IPO,
         price: item.Price !== "NA" ? item.Price : "--",
-        gmp: item.GMP ? item.GMP.replace(/<[^>]*>/g, "") : "",
-        estListing: item["Est Listing"] ? item["Est Listing"].replace(/<[^>]*>/g, "") : "",
+        gmp: gmp,
+        estListing: listingPrice,
         ipoSize: item["IPO Size"] ? item["IPO Size"].replace(/&#8377;/g, "Rs") : "",
         lot: item.Lot,
         open: item.Open,
@@ -25,41 +29,6 @@ router.get("/", async (req, res) => {
         href: item["~URLRewrite_Folder_Name"] ?item["~URLRewrite_Folder_Name"].replace("/gmp/", "") : "",
       };
     });
-
-    // Fetch additional data from mainboard and SME APIs
-    // const [mainboardResponse, smeResponse] = await Promise.all([
-    //   axios.get("https://ipo-tec-app-api.vercel.app/app/mainboard?type=all"),
-    //   axios.get("https://ipo-tec-app-api.vercel.app/app/sme?type=all"),
-    // ]);
-
-    // const additionalData = [
-    //   ...mainboardResponse.data.data,
-    //   ...smeResponse.data.data,
-    // ];
-
-    // // Match and add additional href if available
-    // ipoData.forEach((ipo) => {
-    //   const ipoWords = ipo.companyName.toLowerCase().split(" ");
-
-    //   const phrases = [];
-    //   for (let i = 0; i < ipoWords.length; i++) {
-    //     if (i + 1 < ipoWords.length) {
-    //       phrases.push(`${ipoWords[i]} ${ipoWords[i + 1]}`);
-    //     }
-    //     if (i + 2 < ipoWords.length) {
-    //       phrases.push(`${ipoWords[i]} ${ipoWords[i + 1]} ${ipoWords[i + 2]}`);
-    //     }
-    //   }
-
-    //   const match = additionalData.find((item) => {
-    //     const itemName = item.name.toLowerCase();
-    //     return phrases.some((phrase) => itemName.includes(phrase));
-    //   });
-
-    //   if (match) {
-    //     ipo.href = match.href;
-    //   }
-    // });
 
     res.json({ success: true, data: ipoData });
   } catch (error) {
